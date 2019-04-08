ea1c97edd6a487c673f557c7ad11d7444252349f
Joshua Martinez <joshkmartinez@gmail.com>
2019-04-07 18:39:16 -0700

    added water.css :wink

--- pages/_document.js
+++ pages/_document.js
@@ -35,6 +35,10 @@ export default class extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.css"
+          />
         </Head>
         <body>
           <Main />
